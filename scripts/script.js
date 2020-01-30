93e69c450644b90d026b8f75a4df447812840ae3
Osman Zakir <osmanzakir90@hotmail.com>
2020-01-30 17:57:10 +0500

    finished JS code

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,14 @@
 "use strict";
 
 function parse(expr) {
-  const regex = /(-?\d+(?:\.\d+)?)\s?([+\-*/])\s?(-?\d+(?:\.\d+)?)/;
+  const regex = /(-?\d+(?:\.\d+)?)\s*([+\-*/])\s*(-?\d+(?:\.\d+)?)/;
 
   const results = expr.match(regex);
-
-  const operand1 = results[1];
-  const operator = results[2];
-  const operand2 = results[3];
-
-  const finalResults = [operand1, operator, operand2];
-  return finalResults;
+  if (results) {
+    const [, ...returnResults] = results;
+    return returnResults;
+  }
+  throw new Error("Invalid expression or no expression found");
 }
 
 alert(parse("1.2 * 3.4"));
